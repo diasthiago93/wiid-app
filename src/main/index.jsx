3b590c7d2bcf56f21f26629b0cb992d2de16630e
unknown <thiagohd93@gmail.com>
2021-03-13 10:36:54 -0300

    first commit

---
+++ src/main/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from "./style";
+
+const Main = () => <S.Titulo>Teste</S.Titulo>;
+
+export default Main;
---
+++ src/main/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Titulo = styled.h1`
+  width: 100%;
+  height: 100%;
+  color: red;
+`;
